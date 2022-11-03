69b1a4f3c2a171792c00d3ba2d2135c052a12658
FarruxRavshanov <farruxlynx@gmail.com>
2022-11-03 20:29:54 +0300

    columns

--- find_number_of_columns.py
+++ find_number_of_columns.py
@@ -6,6 +6,16 @@ def find_number_of_columns(data):
     Return:
         int: Number of columns.
     """
-    return
+    count = 0
+    i = 0
+    while i < len(data.split('\n')):
+        count += 1
+        i += 1
+        return count
 
 # Read the csv file
+import csv
+f = open('data.csv')
+reader = csv.reader(f)
+for row in reader:
+    print(row)
